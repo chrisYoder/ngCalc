4cf4661059b0af476e84710a36d7cd6d92a42199
chrisYoder <christophermyoder@gmail.com>
2020-01-28 18:27:37 +0000

    end of regular calc. I will add in scientific calc later

---
+++ ngCalcFolder/src/app/sci-calc/sci-calc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sci-calc',
+  templateUrl: './sci-calc.component.html',
+  styleUrls: ['./sci-calc.component.css']
+})
+export class SciCalcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
